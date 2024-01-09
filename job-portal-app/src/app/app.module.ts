efbc7197bb6f2564b9030914880557b0237a33ba
Agon Jakupi <jakupigoni123@hotmail.com>
2024-01-10 00:10:40 +0100

    Login Component Finished

---
+++ job-portal-app/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes'
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component'
+import { HomeComponent } from './home/home.component'
+import { HeaderComponent } from './header/header.component'
+import { JobsComponent } from './jobs/jobs.component'
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    JobsComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- job-portal-app/src/app/app.routes.ts
+++ job-portal-app/src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'jobs', component: JobsComponent },
+  { path: '**', component: LoginComponent }
+];
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [RouterModule.forRoot(routes), FormsModule],
+  exports: [RouterModule]
+
+})
+
+export class AppRoutingModule { }
